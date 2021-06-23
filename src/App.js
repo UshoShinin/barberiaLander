c76ad0156cb41e8be39ac88292e7e986cafbe089
Akachichi <orradrefederico@gmail.com>
2021-06-23 02:48:31 -0300

    Casi todas las rutas y links a ellas
    Se agregaron casi todas las rutas que vamos a utilizar y estan casi todos los links a ellas. Esto es algo provisorio que se deja medio adelantado porque se va a utilizar mas adelante.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { useState, useReducer, useRef, useEffect } from "react";
-import { NavLink, Route } from "react-router-dom";
+import { useState, useReducer, useRef, useEffect, useContext } from "react";
+import { NavLink, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Button from "./components/Button";
 import Input from "./components/Input";
-import ComboBox from './components/ComboBox/ComboBox';
-import Switch from './components/Switch/Switch';
+import ComboBox from "./components/ComboBox/ComboBox";
+//import Switch from "./components/Switch/Switch";
 import LoaddingSpinner from "./components/LoaddingSpinner/LoaddingSpinner";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 import Expenses from "./components/Lista/Expenses";
-import Modal from './components/UI/Modal/Modal';
-import Backdrop from './components/UI/Backdrop/Backdrop'
+import Modal from "./components/UI/Modal/Modal";
+import Backdrop from "./components/UI/Backdrop/Backdrop";
 import Calendario from "./components/Calendario/Calendario";
+import ContextUsuario from "./context/contextUsuario";
+//IMPORTS PARA LAS PAGINAS
+import Inicio from "./paginas/Inicio";
+import Administracion from "./paginas/Administracion";
+import Cuponeras from "./paginas/Cuponeras";
+import Productos from "./paginas/Productos";
+import Empleados from "./paginas/Empleados";
+import Slider from "./paginas/Slider";
+import AperturaCierre from "./paginas/Caja/AperturaCierre";
+import CalculoJornal from "./paginas/Caja/CalculoJornal";
+import Historial from "./paginas/Caja/Historial";
+import MovimientoCaja from "./paginas/Caja/MovimientoCaja";
+import CrearAgenda from "./paginas/Agenda/CrearAgenda";
+import PreAgendas from "./paginas/Agenda/PreAgendas";
+import VisualAgendas from "./paginas/Agenda/VisualAgendas";
+import NoEncontrado from "./paginas/NoEncontrado";
 
 const inputReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
@@ -51,17 +67,15 @@ function App() {
   };
 
   const resetIsValid = () => {
-    dispatchInput({ type: 'RESET_IS_VALID' });
-  }
+    dispatchInput({ type: "RESET_IS_VALID" });
+  };
 
   const switchHandler = () => {
-    setMySwitch(prevState => {
+    setMySwitch((prevState) => {
       console.log(!prevState);
-      return !prevState
-    }
-    );
-
-  }
+      return !prevState;
+    });
+  };
   const submitHandler = (event) => {
     event.preventDefault();
     if (formIsValid) {
@@ -78,11 +92,11 @@ function App() {
 
   const openModal = () => {
     setShowModal(true);
-  }
+  };
 
   const closeModal = () => {
     setShowModal(false);
-  }
+  };
 
   const DUMMY_COMBOBOX = [
     { id: 1, title: "Soy la opcion 1" },
@@ -96,17 +110,139 @@ function App() {
     { id: 9, title: "Soy la opcion 9" },
     { id: 10, title: "Soy la opcion 10" },
     { id: 11, title: "Soy la opcion 11" },
-  ]
+  ];
 
   const initialExpenses = [
-    { id: "e1", title: "Producto1", amount: 200},
-      { id: "e2", title: "Producto2", amount: 300},
-    { id: "e3", title: "Producto3", amount: 20},
-    { id: "e4", title: "Producto4", amount: 500},
+    { id: "e1", title: "Producto1", amount: 200 },
+    { id: "e2", title: "Producto2", amount: 300 },
+    { id: "e3", title: "Producto3", amount: 20 },
+    { id: "e4", title: "Producto4", amount: 500 },
   ];
-  
+
+  //DEJO ACA LO QUE IRIAMOS A USAR PARA CUANDO CONTROLEMOS A DONDE TIENE QUE IR EL USUARIO
+  //ESTO POR AHORA QUEDA NADA MAS, CUANDO ACOMODEMOS LAS COSAS PARA QUE QUEDEN PROLIJAS HAY QUE PONER EL IMPORT DONDE TIENE QUE IR
+  //HAY QUE IMPORTAR useContext y ContextUsuario donde sea que vayamos a ir
+  //DEJO UNA LISTA CON COMPONENTES LINKS QUE SON LOS QUE DEBERIAMOS USAR EN EL HEADER, SE VAN A RENDERIZAR SEGUN EL ROL
+  const ctxUsuario = useContext(ContextUsuario);
+  const estaLogueado = ctxUsuario.estaLogueado;
+  const rol = ctxUsuario.rol;
+
   return (
     <div className="App">
+      {/*ESTAS SON LAS RUTAS POSIBLES A GRANDES RASGOS*/}
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/inicio" />
+        </Route>
+        <Route path="/inicio">
+          <Inicio />
+        </Route>
+        <Route path="/administracion">
+          <Administracion />
+        </Route>
+        <Route path="/administracion/cuponeras" exact>
+          <Cuponeras />
+        </Route>
+        <Route path="/administracion/empleados" exact>
+          <Empleados />
+        </Route>
+        <Route path="/administracion/productos" exact>
+          <Productos />
+        </Route>
+        <Route path="/administracion/slider" exact>
+          <Slider />
+        </Route>
+        <Route path="/administracion/agenda/crearagenda" exact>
+          <CrearAgenda />
+        </Route>
+        <Route path="/administracion/agenda/preagendas" exact>
+          <PreAgendas />
+        </Route>
+        <Route path="/administracion/agenda/visualagendas" exact>
+          <VisualAgendas />
+        </Route>
+        <Route path="/administracion/caja/aperturacierre" exact>
+          <AperturaCierre />
+        </Route>
+        <Route path="/administracion/caja/calculojornal" exact>
+          <CalculoJornal />
+        </Route>
+        <Route path="/administracion/caja/historial" exact>
+          <Historial />
+        </Route>
+        <Route path="/administracion/caja/movimientocaja" exact>
+          <MovimientoCaja />
+        </Route>
+        <Route path="*">
+          <NoEncontrado />
+        </Route>
+      </Switch>
+
+      {/*ESTA ES LA LISTA QUE HAY DE LOS LINKS CON LOS CONTROLES*/}
+      <ul>
+        <li>
+          <NavLink to="/inicio">Inicio</NavLink>
+        </li>
+        {!estaLogueado && (
+          <li>Iniciar sesion</li>
+        )}
+        {estaLogueado && (rol == "Administrador" || rol == "Encargado" || rol == "Empleado") && (
+            <div>
+              <li>
+                <NavLink to="/administracion">Administracion</NavLink>
+              </li>
+              <li>
+                <NavLink to="/administracion/agenda/visualagendas">
+                  Visualizar Agendas
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/administracion/agenda/crearagenda">
+                  Crear Agenda
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/administracion/caja/movimientocaja">
+                  Movimiento de Caja
+                </NavLink>
+              </li>
+            </div>
+        )}
+        {estaLogueado && rol == "Encargado" && (
+          <div>
+            <li>
+              <NavLink to="/administracion/cuponeras">Cuponeras</NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/empleados">Empleados</NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/slider">Slider</NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/agenda/preagendas">
+                Pre Agendas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/caja/aperturacierre">
+                Abrir/Cerra Caja
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/caja/calculojornal">
+                Calcular Jornal
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/administracion/caja/historial">
+                Historial de Cajas
+              </NavLink>
+            </li>
+          </div>
+        )}
+      </ul>
+
       {/* <form onSubmit={submitHandler} className='fill-window'>
         <div style={{display:'flex',placeItems:'center',alignItems:'center',justifyContent: 'center',height:'100%'}}>
           <Input
@@ -133,14 +269,10 @@ function App() {
       </form>
       <Modal show={showModal} closed={closeModal}/>
       {showModal&& <Backdrop show={showModal}/>} */}
-      <Calendario/>
+      <Calendario />
       {/* <Footer/> */}
     </div>
   );
 }
-
-
-
-
 
 export default App;
