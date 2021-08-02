3b833a8aa1dd216b61f883398a4a572c39f78ec7
brunete1990 <brunete1990@gmail.com>
2021-08-02 20:06:41 -0300

    Navbar

--- cliente/barberiaLander/src/components/Navbar.js
+++ cliente/barberiaLander/src/components/Navbar.js
@@ -1,130 +1,33 @@
+
 import React , {useEffect} from 'react'
 import './Navbar.css';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import $ from 'jquery';
 
 const Navbar = () => {
 
-  function animation(){
-    var tabsNewAnim = $('#navbarSupportedContent');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimTop = activeItemNewAnim.position();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-    $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
-    $("#navbarSupportedContent").on("click","li",function(e){
-      $('#navbarSupportedContent ul li').removeClass("active");
-      $(this).addClass('active');
-      var activeWidthNewAnimHeight = $(this).innerHeight();
-      var activeWidthNewAnimWidth = $(this).innerWidth();
-      var itemPosNewAnimTop = $(this).position();
-      var itemPosNewAnimLeft = $(this).position();
-      $(".hori-selector").css({
-        "top":itemPosNewAnimTop.top + "px", 
-        "left":itemPosNewAnimLeft.left + "px",
-        "height": activeWidthNewAnimHeight + "px",
-        "width": activeWidthNewAnimWidth + "px"
-      });
-    });
-  }
-
-  useEffect(() => {  
-    animation();
-    $(window).on('resize', function(){
-      setTimeout(function(){ animation(); }, 500);
-    });
-    
-  }, []);
-
   return (
-  <nav className="navbar navbar-expand-lg navbar-mainbg">
-    
-      <NavLink className="navbar-brand navbar-logo" to="/" exact>
-        Lander
-      </NavLink>
-    
-    
-      <button 
-        className="navbar-toggler"
-        onClick={ function(){
-          setTimeout(function(){ animation(); });
-        }}
-        type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i className="fas fa-bars text-white"></i>
-      </button>
+    <nav className="navbar-ul">
+      <li><Link to="/" className="navbar-brand">Lander</Link></li> 
+
+      <li><NavLink exact activeClassName="active"  to="/inicio" className="nav-item nav-link">Inicio</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/caja/historial" className="nav-item nav-link">Historial de Cajas</NavLink>  </li>
+      <li><NavLink exact activeClassName="active"  to="/caja/calculojornal"className="nav-item nav-link">Calcular Jornal</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/caja/aperturacierre" className="nav-item nav-link">Abrir/Cerra Caja</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/agenda/preagendas" className="nav-item nav-link">Pre Agendas</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/slider" className="nav-item nav-link">Slider</NavLink ></li>
+      <li><NavLink exact activeClassName="active"  to="/empleados" className="nav-item nav-link">Empleados</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/cuponeras" className="nav-item nav-link">Cuponeras</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/agenda/crearagenda" className="nav-item nav-link">Crear Agenda</NavLink></li>
+      <li><NavLink exact activeClassName="active"  to="/caja/movimientocaja" className="nav-item nav-link">Movimiento de Caja</NavLink>    </li>        
+      <li><NavLink exact activeClassName="active"  to="/agenda/visualagendas" className="nav-item nav-link">Visualizar Agendas</NavLink>   </li>         
+      <li><NavLink exact activeClassName="active"  to="/administracion" className="nav-item nav-link">Administracion</NavLink>           </li> 
+      
+
+    </nav>
  
-      <div 
-        className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ml-auto">
-            
-            <div className="hori-selector">
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-            
-            <li className="nav-item active">
-            <NavLink to="/inicio">Inicio</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/administracion">Administracion</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/agenda/visualagendas">
-              Visualizar Agendas
-            </NavLink>
-          </li>
-          
-          <li className="nav-item">
-            <NavLink to="/caja/movimientocaja">
-              Movimiento de Caja
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/agenda/crearagenda">
-              Crear Agenda
-            </NavLink>
-          </li>
-          <li className="nav-item"> 
-            <NavLink to="/cuponeras">Cuponeras</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/empleados">Empleados</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/slider">Slider</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/agenda/preagendas">
-              Pre Agendas
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/caja/aperturacierre">
-              Abrir/Cerra Caja
-            </NavLink>
-            <ul>
-            <li className="nav-item">
-            <NavLink to="/caja/calculojornal">
-              Calcular Jornal
-            </NavLink>
-          </li>
-            </ul>
-          </li>
-          
-          <li className="nav-item">
-            <NavLink to="/caja/historial">
-              Historial de Cajas
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-  </nav>
   )
 }
 export default Navbar;
+
+
